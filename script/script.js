e4739f7d7d31e3a43c6b018f05520aae6fcb4d1b
patchell-c <patchell.catherine@gmail.com>
2014-12-12 22:03:13 -0500

    updates

--- script/script.js
+++ script/script.js
@@ -2,11 +2,16 @@ var margin = {t:50,l:50,b:50,r:50},
     width = $('.canvas').width()-margin.l-margin.r,
     height = $('.canvas').height()-margin.t-margin.b;
 
-var dotColor = '#992A31';
+var dotColor = '#BF211E';
+var dotColor2 = '#F4FEC1';
+var dotColor3 = '#E7E247';
 var dotColorSet = true;
 var week = ["Jun 28", "Jun 29", "Jun 30", "Jul 01", "Jul 02", "Jul 03", "Jul 04", "Jul 05", "Jul 06", "Jul 07", "Jul 08"];
-var colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'black', 'brown', 'white', '#323232', '#28ea39'];
+var colorArray1 = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'black', 'brown', 'white', '#red', '#28ea39'];
+var colorArray = [dotColor3, dotColor2, dotColor3, dotColor2, dotColor3, dotColor2, dotColor3, dotColor2, dotColor3, dotColor2, dotColor3];
 
+var globalTest;
+var lonLats;
 
 /* ----- Setting up D3 Projection, Path, and Zoom ----- */
 var projection = d3.geo.albersUsa()
@@ -22,7 +27,18 @@ var zoom = d3.behavior.zoom()
     //.scaleExtent([1, 8])
     .on("zoom", zoomed);
 
-var colorRange = d3.scale.linear().domain([0,0.15]).range(["#fff","red"]);
+var colorRange = d3.scale.linear().domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]).range(colorArray);
+
+var lineGenerator = d3.svg.line()
+    .x(function(d){
+        var proj = projection([d[0], d[1]]);
+        return proj[0];
+    })
+    .y(function(d){
+        var proj = projection([d[0], d[1]]);
+        return proj[1];
+    })
+    .interpolate('linear');
 
 
 /* ----- Setting up svg and canvas -----*/
@@ -41,7 +57,6 @@ svg.append("rect")
     .attr("height", height)
     .call(zoom);
 
-
 /* ----- Importing data ----- */
 queue()
     .defer(d3.json, "data/gz_2010_us_050_00_5m.json")
@@ -53,21 +68,43 @@ queue()
         draw(counties, states);
 
         //draw location points once data is finished loading
-        parseKml(function(places) { drawPoints(places); });
+        parseKml(function(places) { drawPoints(places) });
 
         //set up click events
         $('#plot').on('click', function(e){
             dotColorSet = true;
-            drawPoints(places);
+            drawPoints(globalTest);
          });
      
         $('#plot_color').on('click', function(e){
             dotColorSet = false;
-            console.log("clicked plot_color and dotColorSet=" + dotColorSet);
-            drawPoints(places);
+            drawPoints(globalTest);
         });
 
-        
+        $('#plot_scroll').on('click', function(e){
+            dotColorSet = true;
+            
+        //geojson format attempt 1
+        // var lonLats = { "type": "FeatureCollection",
+        //                     "features": { 
+        //                         "type": "Feature",
+        //                         "geometry": {
+        //                           "type": "LineString",
+        //                           "coordinates": extractPos(globalTest)
+        //                           },
+        //                         "properties": {
+        //                           "prop0": "value0",
+        //                           "prop1": 0.0
+        //                         }
+        //                     }
+        //               };
+
+        //geojson format
+        var lonLats = { "type": "FeatureCollection", "features":  extractPos(globalTest) };
+
+        drawLonLats(lonLats);
+
+        });
 	})
 
 //convert google kml location data (renamed to .xml file) to json
@@ -82,11 +119,23 @@ function parseKml(next){
         //go directly to object level we want
         var data = jsonData.kml.Document.Placemark['gx:Track'];
 
-        data = formatData(data);
+        globalTest = formatData(data);
 
         //callback
-        next(data);
+        next(globalTest);
     });
+}
+
+//return an array consisting of pairs of longitude and latitude coorddinates
+//so as to use in geojson format
+function extractPos(data){
+    var posn = [];
+
+    for (var x = 0; x < data.length; x++){
+        posn.push([ data[x].where[0], data[x].where[1]]); 
+    }
+
+    return posn;
 }
 
 //create objects with fields for corresponding locations and dates
@@ -103,8 +152,25 @@ function formatData(data){
     return dataArray;
 }
 
+//draw the path of longitude/latitude points
+//to be used for drawing the scrolling path
+function drawLonLats(posns){
+
+    console.log(posns);
+
+    features.selectAll("path")
+        .data(posns.features)
+        .enter()
+        .append('path')
+        .attr('class','state')
+        .attr('d', path)
+        .style("stroke-width", "2px")
+        ;
+}
+
 //draw the world, country, and airpoint points on the canvas
 function draw(counties, states){
+    console.log(states);
     var states = features.selectAll('.state')
         .data(states.features, function(d){
             return d.properties.STATE;
@@ -114,11 +180,12 @@ function draw(counties, states){
         .enter()
         .append('path')
         .attr('class','state')
-        .attr('d',path)
+        .attr('d', path)
         .style("stroke-width", "2px");
 }
 
 function drawPoints(places){
+    //check to make sure that places is defined, if not, exit
     console.log(places)
 
     var points = features.selectAll('point')
@@ -130,7 +197,6 @@ function drawPoints(places){
            var proj = projection([d.where[0], d.where[1]]);
            return 'translate('+proj[0]+','+proj[1]+')'
         })
-        .transition()
         .attr('r', '2px')
         .attr('fill', function(d){
             if (dotColorSet){
@@ -138,26 +204,20 @@ function drawPoints(places){
             } else 
             return colorize(d);
         })
-        // .style('fill',function(d){
-        //    var rate = d.when;
-        //    return scaleColor(rate);
-        // })
+        .append('title')
+        .text('this is a tooltip')
         ;
 }
 
 function colorize(d){
-    console.log('hit colorize');
     var date = d.when;
 
     for(var x = 0; x < week.length; x++){
-        if (date.indexOf(week[x]) > -1){
-            console.log("found date " + date + " in " + week[x])
-            return colorArray[x];
+        if (date.toString().indexOf(week[x]) > -1){
+            return colorRange(x);
         }
     }
-    
 }
-
 
 function zoomed() {
   features.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
@@ -170,7 +230,3 @@ function zoomed() {
 }
 
 d3.select(self.frameElement).style("height", height + "px");
-
-
-
-
